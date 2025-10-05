7c2d307a7eb15c1f2bb14b641f8dbd6ba4f7cd56
louinfluence <60574525+louinfluence@users.noreply.github.com>
2025-10-05 18:35:59 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+// --------------------
+// MAIN GAME LOGIC
+// --------------------
+import { rotateHour, rotateMinute, setTime } from "./clock.js";
+
+window.rotateHour = rotateHour;
+window.rotateMinute = rotateMinute;
+window.setTime = setTime;
+
+// Beispiel-Testaufruf:
+setTime(3, 45); // zeigt 3:45
 
